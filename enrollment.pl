231bf9a74e5392a6f6eed978b15f688144edd6bb
Francie Galapate <francie.gabrielle2005@gmail.com>
2026-02-11 21:27:43 +0800

    complete all subs

--- enrollment.pl
+++ enrollment.pl
@@ -25,43 +25,59 @@ units(cs132, 3).
 % second year
 units(cs211, 2).    units(cs211l, 1).
 units(cs212, 2).    units(cs212l, 1).
-units(cs213, 3).
+units(cs213, 3).    units(gsts, 3).
+units(grva, 3).     units(nstpcwts1, 3).
+units(fitoa, 2).    units(cfe103, 3).
+
+units(cs231, 2).    units(cs231l, 1).
+units(grizal, 3).   units(gentrep, 3).
 
 units(cs221, 2).    units(cs221l, 1).
 units(cs222, 2).    units(cs222l, 1).
-units(cs223, 3).
-
-units(cs231, 2).
-units(cs231l, 1).
+units(cs223, 3).    units(gethics, 3).
+units(gcworld, 3).  units(nstpcwts2, 3).
+units(fitaq, 2).    units(cfe104, 3).
 
 % third year
 units(cs311, 2).    units(cs311l, 1).
 units(cs312, 2).    units(cs312l, 1).
 units(cs313, 3).    units(cs314, 3).
 units(cs315, 3).    units(csm316, 3).
+units(cfe105a, 1.5).
 
 units(cs321, 3).    units(cs322, 2).
 units(cs322l, 1).   units(cs323, 2).
 units(cs323l, 1).   units(cs324, 3).
-units(cs325, 3).
+units(cs325, 3).    units(cfe105b, 1.5).
 
 units(cs331, 6).
 
 % fourth year
+units(cfe106a, 1.5).
 units(cs411, 3).
 units(cs412, 3).
 units(cs413, 3).
 units(cse1, 3).
 units(cse2, 3).
 
+units(cfe106b, 1.5).
 units(cs421, 3).
 units(cs422, 3).
+units(for_lang1, 3).
 units(cse3, 3).
 units(cse4, 3).
 
+% cs electives
+units(cse10, 3). units(cse11, 3). units(cse12, 3). units(cse13, 3).
+units(cse14, 3). units(cse15, 3). units(cse16, 3). units(cse17, 3).
+units(cse18, 3). units(cse19, 3). units(cse20, 3). units(cse21, 3).
+units(cse22, 3). units(cse23, 3). units(cse24, 3). units(cse25, 3).
+units(cse26, 3). units(cse27, 3). units(cse28, 3). units(cse29, 3).
+units(cse30, 3). units(cse31, 3). units(cse32, 3).
+
 % PREREQUISITES - prereq(course, list of prerequisites)
 
-% first year first sem
+% first year
 prereq(cs111, []).   prereq(cs111l, []).
 prereq(cs112, []).   prereq(cs112l, []).
 prereq(cs113, []).
@@ -71,7 +87,6 @@ prereq(ghist, []).
 prereq(fithw, []).
 prereq(cfe101, []).
 
-% first year second sem
 prereq(cs121, [cs111]).
 prereq(cs122, [cs112]).  prereq(cs122l, [cs112]).
 prereq(cs123, [cs112]).  prereq(cs123l, [cs112]).
@@ -81,49 +96,85 @@ prereq(genvi, []).
 prereq(fitcs, []).
 prereq(cfe102, []).
 
-% first year short term
 prereq(cs131, [cs111]).
 prereq(cs132, []).
 
-% second year first sem
-prereq(cs211,[cs112,cs113]).
-prereq(cs211l,[cs112,cs113]).
-prereq(cs212,[cs123]).
-prereq(cs212l,[cs123]).
-prereq(cs213,[cs122]).
+% second year
+prereq(cs211, [cs112, cs113, cs212]).
+prereq(cs211l, [cs112, cs113, cs212]).
+prereq(cs212, [cs123, cs211]).
+prereq(cs212l, [cs123, cs211]).
+prereq(cs213, [cs122]).
+prereq(gsts, []).
+prereq(grva, []).
+prereq(nstpcwts1, []).
+prereq(fitoa, []).
+prereq(cfe103, []).
 
-% second year second sem
-prereq(cs221,[cs211]).
-prereq(cs221l,[cs211]).
-prereq(cs222,[cs122]).
-prereq(cs222l,[cs122]).
-prereq(cs223,[cs132]).
+prereq(cs221, [cs211]).
+prereq(cs221l, [cs211]).
+prereq(cs222, [cs122]).
+prereq(cs222l, [cs122]).
+prereq(cs223, [cs132]).
+prereq(gethics, []).
+prereq(gcworld, []).
+prereq(nstpcwts2, [nstpcwts1]).
+prereq(fitaq, []).
+prereq(cfe104, [cfe103]).
 
-% second year short term
-prereq(cs231,[cs212]).
-prereq(cs231l,[cs212]).
+prereq(cs231, [cs212]).
+prereq(cs231l, [cs212]).
+prereq(grizal, []).
+prereq(gentrep, []).
 
-% third year first sem
-prereq(cs311,[cs122]).
-prereq(cs311l,[cs122]).
-prereq(cs312,[cs211]).
-prereq(cs312l,[cs211]).
-prereq(cs313,[cs131,cs221]).
-prereq(cs314,[cs111,gself]).
-prereq(cs315,[cs111,gpcom]).
-prereq(csm316,[cs132]).
+% third year
+prereq(cs311, [cs122]).
+prereq(cs311l, [cs122]).
+prereq(cs312, [cs211]).
+prereq(cs312l, [cs211]).
+prereq(cs313, [cs131, cs221]).
+prereq(cs314, [cs111, gself]).
+prereq(cs315, [cs111, gpcom]).
+prereq(csm316, [cs132]).
+prereq(cfe105a, [cfe103, cfe104]).
 
-% third year second sem
-prereq(cs321,[cs132,cs211]).
-prereq(cs322,[cs221]).
-prereq(cs322l,[cs221]).
-prereq(cs323,[cs132,cs211]).
-prereq(cs323l,[cs132,cs211]).
-prereq(cs325,[cs211]).
+prereq(cs321, [cs132, cs211]).
+prereq(cs322, [cs221]).
+prereq(cs322l, [cs221]).
+prereq(cs323, [cs132, cs211]).
+prereq(cs323l, [cs132, cs211]).
 prereq(cs324, [third_year_standing]).
+prereq(cs325, [cs211]).
+prereq(cfe105b, [cfe105a]).
 
-% third year short term
-prereq(cs331, [fourth_year_standing]).
+% fourth year
+prereq(cfe106a, [cfe105b]).
+prereq(cs411, [cs324]).
+prereq(cs412, [third_year_standing]).
+prereq(cs413, [gethics, cs231, cs221]).
+prereq(cse1, [third_year_standing]).
+prereq(cse2, [third_year_standing]).
+
+prereq(cfe106b, [cfe106a]).
+prereq(cs421, [cs411]).
+prereq(cs422, [fourth_year_standing]).
+prereq(for_lang1, []).
+prereq(cse3, [fourth_year_standing]).
+prereq(cse4, [fourth_year_standing]).
+
+% cs electives
+prereq(cse10, [third_year_standing]). prereq(cse11, [third_year_standing]).
+prereq(cse12, [third_year_standing]). prereq(cse13, [third_year_standing]).
+prereq(cse14, [third_year_standing]). prereq(cse15, [third_year_standing]).
+prereq(cse16, [third_year_standing]). prereq(cse17, [third_year_standing]).
+prereq(cse18, [third_year_standing]). prereq(cse19, [third_year_standing]).
+prereq(cse20, [third_year_standing]). prereq(cse21, [third_year_standing]).
+prereq(cse22, [third_year_standing]). prereq(cse23, [third_year_standing]).
+prereq(cse24, [third_year_standing]). prereq(cse25, [third_year_standing]).
+prereq(cse26, [third_year_standing]). prereq(cse27, [third_year_standing]).
+prereq(cse28, [third_year_standing]). prereq(cse29, [third_year_standing]).
+prereq(cse30, [third_year_standing]). prereq(cse31, [third_year_standing]).
+prereq(cse32, [third_year_standing]).
 
 calculate_total_units([], 0).
 calculate_total_units([Course|Tail], Total) :-
@@ -192,3 +243,4 @@ process_course(_, DesiredCourse) :-
 % query: check_eligibility([cs111,cs111l, cs112, cs112l, cs113], [cs211, cs212]).
 % query: check_eligibility([cs111,cs111l], cs121).
 % query (3rd year standing): check_eligibility([cs111, cs111l, cs112, cs112l, cs113, cs121, cs122, cs122l, cs123, cs123l, cs131, cs132, cs211, cs211l, cs212, cs212l, cs213, cs221, cs221l, cs222, cs222l, cs223, cs231, cs231l], [cs324,cs315]).
+% query (cs elective): check_eligibility([cs111, cs111l, cs112, cs112l, cs113, gmath, gart, ghist, fithw, cfe101, cs131, cs132, cs121, cs122, cs122l, cs123, cs123l, gself, gpcom, genvi, fitcs, cfe102, cs211, cs211l, cs212, cs212l, cs213, gsts, grva, nstpcwts1, fitoa, cfe103, cs231, cs231l, grizal, gentrep, cs221, cs221l, cs222, cs222l, cs223, gethics, gcworld, nstpcwts2, fitaq, cfe104], cse10).
